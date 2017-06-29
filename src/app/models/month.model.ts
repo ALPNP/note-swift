af81ea22e485f4a67c81aa42a68207680950dbb5
Filippov <filippov@teleformis.ru>
2017-06-29 23:40:25 +0300

    angular here

---
+++ src/app/models/month.model.ts
@@ -0,0 +1,9 @@
+export class Month {
+    days: number;
+    monthPosition: number;
+
+    constructor(params: any){
+        this.days = params.days;
+        this.monthPosition = params.monthPosition;
+    }
+}
