0e750c5905227d7b8bdfe4af6268997e46e2da3b
Filippov <filippov@teleformis.ru>
2017-07-08 15:19:33 +0300

    integrate material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { CalendarComponent} from './components/calendar/calendar.component';
+import {MdMenuModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
+
+import {AppComponent} from './app.component';
+import {MainMenuComponent} from './components/main-menu/main-menu.component';
+import {CalendarComponent} from './components/calendar/calendar.component';
+
+import 'hammerjs';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalendarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CalendarComponent,
+        MainMenuComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdMenuModule,
+        MdButtonModule,
+        MdIconModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
